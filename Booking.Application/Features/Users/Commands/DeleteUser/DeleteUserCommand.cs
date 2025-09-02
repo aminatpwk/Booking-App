bec3183f9a6089ef2fae8135bd7ba8fb0e80f559
unknown <aminasokoli0602@outlook.com>
2025-09-02 23:11:50 +0200

    added DeleteUser operation

---
+++ Booking.Application/Features/Users/Commands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Application.Features.Users.Commands.DeleteUser
+{
+    public class DeleteUserCommand : IRequest<Guid>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ Booking.Application/Features/Users/Commands/DeleteUser/DeleteUserHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Application.Features.Users.Commands.DeleteUser
+{
+    public class DeleteUserHandler : IRequestHandler<DeleteUserCommand,Guid>
+    {
+        private readonly IUserRepository _userRepository;
+        public DeleteUserHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Guid> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetById(request.Id);
+            if(user == null)
+            {
+                throw new Exception($"User with ID {request.Id} not found!");
+            }
+            await _userRepository.Delete(user);
+            return user.Id;
+        }
+    }
+}
